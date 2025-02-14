9881f2ae5713bfcd9f1272c7c07abfa6f45195cf
Antoine Hazebrouck <antoine.haz@gmail.com>
2025-02-14 11:46:22 +0100

    removed local profile for cucumber tests

--- authentication/src/test/java/fr/imt/authentication/CucumberWithSpringBootContext.java
+++ authentication/src/test/java/fr/imt/authentication/CucumberWithSpringBootContext.java
@@ -2,11 +2,10 @@ package fr.imt.authentication;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.ActiveProfiles;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 
-@ActiveProfiles("local") // TODO remove
+// @ActiveProfiles("local")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @CucumberContextConfiguration
 public final class CucumberWithSpringBootContext {
