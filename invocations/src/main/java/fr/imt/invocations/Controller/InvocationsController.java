5a5f87fdde8ae5d541421da40dbe15fb425e6994
Sontag Maxime <maxime_sontag@ens.univ-artois.fr>
2025-03-06 15:07:13 +0100

    Invocations API

--- invocations/src/main/java/fr/imt/invocations/Controller/InvocationsController.java
+++ invocations/src/main/java/fr/imt/invocations/Controller/InvocationsController.java
@@ -1,12 +1,13 @@
 package fr.imt.invocations.Controller;
 
 import fr.imt.invocations.Entity.Invocations;
-import fr.imt.invocations.Entity.InvocationsDTO;
 import fr.imt.invocations.Service.InvocationsService;
-import jakarta.validation.Valid;
-import org.aopalliance.intercept.Invocation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,33 +19,63 @@ public class InvocationsController {
     @Autowired
     private InvocationsService invocationsService;
 
-    @GetMapping("/{id}")
-    public Invocations getInvocation(@PathVariable UUID id) {
-        return invocationsService.getInvocationById(id);
+    public final static String AUTHENTICATION_SCHEMA = "http";
+    public final static String AUTHENTICATION_HOST = "authentication-api";
+    public final static int AUTHENTICATION_PORT = 8080;
+    public final static String AUTHENTICATION_PATH = "/check-token";
+
+    RestClient restClient = RestClient.create();
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public class UnauthorizedException extends RuntimeException {
+        public UnauthorizedException(String error) {
+            super(error);
+        }
     }
 
-//    @PostMapping("/new")
-//    public void createInvocation(@Valid @RequestBody InvocationsDTO invocation) {
-//        invocationsService.createInvocation(invocation);
-//    }
+    public boolean isAuthenticated(String token) {
+        ResponseEntity<String> responseEntity = restClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme(AUTHENTICATION_SCHEMA)
+                        .host(AUTHENTICATION_HOST)
+                        .port(AUTHENTICATION_PORT)
+                        .path(AUTHENTICATION_PATH)
+                        .queryParam("token", token)
+                        .build())
+                .retrieve()
+                .onStatus(code -> code.is4xxClientError(), (request, response) -> {
+                    throw new UnauthorizedException(response.getStatusText());
+                })
+                .toEntity(String.class);
+        return responseEntity.getStatusCode() == HttpStatusCode.valueOf(200);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Invocations> getInvocation(@PathVariable UUID id, @RequestHeader String token) {
+        if (!isAuthenticated(token)) return ResponseEntity.status(HttpStatusCode.valueOf(401)).build();
+        return ResponseEntity.ok(invocationsService.getInvocationById(id));
+    }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable int id) {
+    public void delete(@PathVariable int id, @RequestHeader String token) {
         invocationsService.deleteInvocation(id);
     }
 
     @GetMapping("/monstres/{id}")
-    public List<Invocations> getMonstreById(@PathVariable int id) {
-        return invocationsService.getInvocationsByIdMonstre(id);
+    public ResponseEntity<List<Invocations>> getMonstreById(@PathVariable int id, @RequestHeader String token) {
+        if (!isAuthenticated(token)) return ResponseEntity.status(HttpStatusCode.valueOf(401)).build();
+        return ResponseEntity.ok(invocationsService.getInvocationsByIdMonstre(id));
     }
 
     @GetMapping("/joueurs/{id}")
-    public List<Invocations> getJoueurById(@PathVariable int id) {
-        return invocationsService.getInvocationsByIdJoueur(id);
+    public ResponseEntity<List<Invocations>> getJoueurById(@PathVariable int id, @RequestHeader String token) {
+        if (!isAuthenticated(token)) return ResponseEntity.status(HttpStatusCode.valueOf(401)).build();
+        return ResponseEntity.ok(invocationsService.getInvocationsByIdJoueur(id));
     }
 
     @PostMapping("/new/{playerId}")
-    public int invoquerMonstre(@PathVariable int playerId) {
-        return invocationsService.invoquerMonstre(playerId);
+    public ResponseEntity<Integer> invoquerMonstre(@PathVariable int playerId, @RequestHeader String token) {
+        if (!isAuthenticated(token)) return ResponseEntity.status(HttpStatusCode.valueOf(401)).build();
+        return ResponseEntity.ok(invocationsService.invoquerMonstre(playerId));
     }
 }
